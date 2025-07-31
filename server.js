99afd25ec45154584ee6e1a9aaf4a7074f88ef54
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-31 10:50:38 +0530

    routes added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/database');
+const productsRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +13,11 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+
+app.use('/api/products', productsRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
