2b0582aaccd16fb9341cb06566fa4c19286f9559
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-02 18:22:07 +0530

    server file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const connectDB = require('./config/database');
 const productsRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
+const wishlistRoutes = require('./routes/wishlist');
+const cartRoutes = require('./routes/cart');
+const orderRoutes = require('./routes/orders');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,12 +15,16 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static('uploads'));
 
 
 app.use('/api/products', productsRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/wishlist', wishlistRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
